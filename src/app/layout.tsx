78ebe9335595ecad19fff5f8a8a6495aa0402b35
parlamas <parlamas@live.com>
2026-01-04 11:27:21 +0100

    20260104-1127

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./components/AuthProvider";
 import NavBar from "./components/NavBar";
+import MobileMenu from "./components/MobileMenu";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <AuthProvider>
           <NavBar />
+          <MobileMenu />
           {children}
         </AuthProvider>
       </body>
