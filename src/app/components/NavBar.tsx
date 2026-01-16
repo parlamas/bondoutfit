09a09d9f78f338c7e33a6f53dedc4c9807a4d74f
parlamas <parlamas@live.com>
2026-01-16 14:33:10 +0100

    20260116-1433

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -77,7 +77,7 @@ export default function RoleBasedNavbar() {
             href="/dashboard/customer"
             className="rounded-md bg-blue-600 px-3 py-2 text-sm font-medium text-white hover:bg-blue-700"
           >
-            My Visits
+            My Dashboard
           </Link>
         )}
         
